6691eb43aeca1e9a8c7cb9ed7fbbb8ec11f3e51a
memray <memray0@gmail.com>
2017-11-12 00:31:40 -0500

    finish training of seq2seq

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --cluster=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gtx1080
+#SBATCH --partition=titanx
 #SBATCH --job-name=tdr_dag
 #SBATCH --output=tdr_dag.out
 #SBATCH --nodes=1
