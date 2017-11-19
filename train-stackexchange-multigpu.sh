899f0359c4c903d51959e51812d10c1d78ea8b0d
rum20 <rum20@login1.crc.pitt.edu>
2017-11-18 21:29:07 -0500

    edit batch_num for multi-gpu

--- train-stackexchange-multigpu.sh
+++ train-stackexchange-multigpu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --cluster=gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:3
 #SBATCH --partition=titanx
 #SBATCH --job-name=tdr_dag
 #SBATCH --output=tdr_dag.out
@@ -12,4 +12,4 @@
 #module restore
 
 # Run the job
-srun python -m train -data data/stackexchange/stackexchange.train_valid.pt -vocab data/stackexchange/stackexchange.vocab.pt -exp_path "exp/stackexchange.bi-directional.%s" -exp "stackexchange" -batch_size 1024 -bidirectional -gpuid 0 1
+srun python -m train -data data/stackexchange/stackexchange.train_valid.pt -vocab data/stackexchange/stackexchange.vocab.pt -exp_path "exp/stackexchange.bi-directional.%s" -exp "stackexchange" -batch_size 1024 -bidirectional -gpuid 0 1 2
