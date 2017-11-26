320ead825120f79266de21c44ae42a9934f37531
memray <memray0@gmail.com>
2017-11-25 21:41:57 -0500

    more bacthes for teacher forcing

--- srun_test.sh
+++ srun_test.sh
@@ -3,6 +3,6 @@
 #SBATCH --error=test.err
 #SBATCH -t 00:10:00
 
-EXP_NAME = "rnn.scheduled_sampling"
+srun EXP_NAME = "rnn.scheduled_sampling"
 srun echo "Hello $(hostname)"
 srun echo "Hello $(EXP_NAME)"
