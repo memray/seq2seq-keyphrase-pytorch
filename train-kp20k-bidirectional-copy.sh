6b27eebcf4ef0810f8a58dae90589326dbf06f59
memray <memray0@gmail.com>
2017-12-11 21:13:50 -0500

    fix errors in script

--- train-kp20k-bidirectional-copy.sh
+++ train-kp20k-bidirectional-copy.sh
@@ -15,4 +15,4 @@
 # Run the job
 export EXP_NAME="rnn.teacher_forcing.copy"
 export DATA_NAME="kp20k"
-srun python -m train -data data/$DATA_NAME/$DATA_NAME vocab data/$DATA_NAME/$DATA_NAME.vocab.pt -exp_path "exp/$EXP_NAME/%s.%s" -save_path "model/$EXP_NAME/%s.%s" -exp "$DATA_NAME" -batch_size 256 -bidirectional -copy_model -run_valid_every 2000 -save_model_every 2000 -must_teacher_forcing -beam_size 16 -beam_search_batch_size 32
+srun python -m train -data data/$DATA_NAME/$DATA_NAME -vocab data/$DATA_NAME/$DATA_NAME.vocab.pt -exp_path "exp/$EXP_NAME/%s.%s" -save_path "model/$EXP_NAME/%s.%s" -exp "$DATA_NAME" -batch_size 256 -bidirectional -copy_model -run_valid_every 2000 -save_model_every 2000 -must_teacher_forcing -beam_size 16 -beam_search_batch_size 32
