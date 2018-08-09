7922dd6b7256f7bca9c0d1067adbaa806ce4bde8
Eric Yuan <ericyuan@DESKTOP-00P4VTC.northamerica.corp.microsoft.com>
2018-08-09 14:46:20 -0400

    simplify model load/save

--- train_cas.py
+++ train_cas.py
@@ -290,11 +290,11 @@ def train_model(model, optimizer_ml, optimizer_rl, criterion, train_data_loader,
                     logging.info('Saving checkpoint to: %s' % os.path.join(opt.model_path, '%s.epoch=%d.batch=%d.total_batch=%d.error=%f' % (opt.exp, epoch, batch_i, total_batch, valid_loss) + '.model'))
                     torch.save(
                         model.state_dict(),
-                        open(os.path.join(opt.model_path, '%s.epoch=%d.batch=%d.total_batch=%d' % (opt.exp, epoch, batch_i, total_batch) + '.model'), 'wb')
+                        os.path.join(opt.model_path, '%s.epoch=%d.batch=%d.total_batch=%d' % (opt.exp, epoch, batch_i, total_batch) + '.model')
                     )
                     torch.save(
                         (epoch, total_batch, best_loss, stop_increasing, checkpoint_names, train_ml_history_losses, valid_history_losses, test_history_losses),
-                        open(os.path.join(opt.model_path, '%s.epoch=%d.batch=%d.total_batch=%d' % (opt.exp, epoch, batch_i, total_batch) + '.state'), 'wb')
+                        os.path.join(opt.model_path, '%s.epoch=%d.batch=%d.total_batch=%d' % (opt.exp, epoch, batch_i, total_batch) + '.state')
                     )
 
                 if stop_increasing >= opt.early_stop_tolerance:
@@ -410,26 +410,10 @@ def init_model(opt):
 
     if opt.train_from:
         logging.info("loading previous checkpoint from %s" % opt.train_from)
-        # load the saved the meta-model and override the current one
-        model = torch.load(
-            open(os.path.join(opt.model_path, opt.exp, '.initial.model'), 'wb')
-        )
-
         if torch.cuda.is_available():
-            checkpoint = torch.load(open(opt.train_from, 'rb'))
+            model.load_state_dict(torch.load(opt.train_from))
         else:
-            checkpoint = torch.load(
-                open(opt.train_from, 'rb'), map_location=lambda storage, loc: storage
-            )
-        # some compatible problems, keys are started with 'module.'
-        checkpoint = dict([(k[7:], v) if k.startswith('module.') else (k, v) for k, v in checkpoint.items()])
-        model.load_state_dict(checkpoint)
-    else:
-        # dump the meta-model
-        torch.save(
-            model.state_dict(),
-            open(os.path.join(opt.train_from[: opt.train_from.find('.epoch=')], 'initial.model'), 'wb')
-        )
+            model.load_state_dict(torch.load(opt.train_from, map_location={'cuda:0': 'cpu'}))
 
     if torch.cuda.is_available():
         model = model.cuda()
@@ -473,15 +457,7 @@ def process_opt(opt):
     logging.info('EXP_PATH : ' + opt.exp_path)
 
     # dump the setting (opt) to disk in order to reuse easily
-    if opt.train_from:
-        opt = torch.load(
-            open(os.path.join(opt.model_path, opt.exp + '.initial.config'), 'rb')
-        )
-    else:
-        torch.save(opt,
-                   open(os.path.join(opt.model_path, opt.exp + '.initial.config'), 'wb')
-                   )
-        json.dump(vars(opt), open(os.path.join(opt.model_path, opt.exp + '.initial.json'), 'w'))
+    json.dump(vars(opt), open(os.path.join(opt.model_path, opt.exp + '.initial.json'), 'w'))
 
     return opt
 
