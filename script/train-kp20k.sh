807085368e69e0212eacf292076a58495da47d6d
memray <memray0@gmail.com>
2018-09-26 01:46:48 -0400

    run on CRC

--- script/train-kp20k.sh
+++ script/train-kp20k.sh
@@ -20,4 +20,4 @@ export EXP_NAME="rnn.kp20k.multi_test.general"
 export ATTENTION="general"
 export ROOT_PATH="/zfs1/pbrusilovsky/rum20/seq2seq-keyphrase-pytorch"
 export DATA_NAME="kp20k"
-srun python -m train data_path_prefix data/$DATA_NAME/$DATA_NAME -vocab_path data/$DATA_NAME/$DATA_NAME.vocab.pt -exp "$DATA_NAME" -batch_size 128 -bidirectional -run_valid_every 5000 -save_model_every 5000 -beam_size 32 -beam_search_batch_size 3 -train_ml
+srun python -m train -data_path_prefix data/$DATA_NAME/$DATA_NAME -vocab_path data/$DATA_NAME/$DATA_NAME.vocab.pt -exp "$DATA_NAME" -batch_size 128 -bidirectional -run_valid_every 5000 -save_model_every 5000 -beam_size 32 -beam_search_batch_size 3 -train_ml
