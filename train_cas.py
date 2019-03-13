6d9bb8c203f98d9302ebde51c6fd147f75a58e4e
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2019-03-13 16:20:24 -0400

    now it's runable

--- train_cas.py
+++ train_cas.py
@@ -224,6 +224,7 @@ def train_model(model, optimizer, criterion, train_data_loader, valid_data_loade
     for epoch in range(config['training']['epochs']):
         progbar = Progbar(logger=logging, title='Training', target=len(train_data_loader), batch_size=train_data_loader.batch_size, total_examples=len(train_data_loader.dataset.examples))
 
+        print('*' * 20)
         print("Training @ Epoch=%d" % (epoch))
         for batch_i, batch in enumerate(tqdm(train_data_loader)):
             model.train()
